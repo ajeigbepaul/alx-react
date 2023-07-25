c086602ba5ba8f17bec069947be09b19841764b3
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-07-25 03:39:24 -0700

    fix2

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,24 +1,5 @@
-// Import jQuery (ensure the correct path to your jQuery file)
-import $ from 'jquery';
+const $ = require('jquery');
 
-// Function to add three paragraphs to the page body
-function addParagraphs() {
-  const body = $('body');
-
-  // Paragraph 1
-  const paragraph1 = $('<p>').text('Holberton Dashboard.');
-  body.append(paragraph1);
-
-  // Paragraph 2
-  const paragraph2 = $('<p>').text('Dashboard data for the students.');
-  body.append(paragraph2);
-
-  // Paragraph 3
-  const paragraph3 = $('<p>').text('Copyright - Holberton School.');
-  body.append(paragraph3);
-}
-
-// Call the function to add paragraphs when the DOM is ready
-$(document).ready(() => {
-  addParagraphs();
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
