acd0d71025955d8d38ae8489aebe5de83c6dafbe
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-08-18 11:14:24 +0100

    fix correction

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,13 +1,23 @@
 export default function accessImmutableObject(object, array) {
-  let value = object;
+  let currentValue = object;
 
   for (const key of array) {
-    if (value && typeof value === "object" && key in value) {
-      value = value[key];
+    if (currentValue && currentValue.hasOwnProperty(key)) {
+      currentValue = currentValue[key];
     } else {
-      return undefined; // Return undefined if the path is invalid
+      return undefined; // Return undefined if the path is not valid
     }
   }
 
-  return value;
+  return currentValue;
 }
+
+const result = accessImmutableObject({
+  name: {
+    first: "Guillaume",
+    last: "Salva"
+  }
+}, ['name', 'first']);
+
+console.log(result); // Output: Guillaume
+
